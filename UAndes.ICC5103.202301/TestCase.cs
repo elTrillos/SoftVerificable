bfb74103aca6ff18f9a9ef7b5379901337c196a9
Ignacio Inostroza <iinostroza1@miuandes.cl>
2023-06-22 18:18:12 -0400

    testcases finished

--- UAndes.ICC5103.202301/TestCase.cs
+++ UAndes.ICC5103.202301/TestCase.cs
@@ -12,7 +12,7 @@ namespace UAndes.ICC5103._202301
     public class TestCase
     {
         [TestCase]
-        public void CheckSumAdquirientes()
+        public void CheckSumAdquirientesPercetanges()
         {
             AdquirienteVerificator verificator = new AdquirienteVerificator();
             List<AdquirienteClass> adquirientelist = new List<AdquirienteClass>
@@ -40,7 +40,7 @@ namespace UAndes.ICC5103._202301
         }
 
         [TestCase]
-        public void ValidPercentage()
+        public void CheckifPercentageSumValid()
         {
             ValuesChecker checker = new ValuesChecker();
             List<AdquirienteClass> adquirientelist = new List<AdquirienteClass>
@@ -68,7 +68,7 @@ namespace UAndes.ICC5103._202301
         }
 
         [TestCase]
-        public void GetLatestMultipropietarioYear_ReturnsLatestYear()
+        public void CheckLatestMultipropietarioYear()
         {
             ValuesChecker checker = new ValuesChecker();
             List<Multipropietario> multipropietarios = new List<Multipropietario>
@@ -108,6 +108,89 @@ namespace UAndes.ICC5103._202301
             int actualYear = checker.GetLatestMultipropietarioYear(multipropietarios);
 
             Assert.AreEqual(expectedYear, actualYear);
+        }
+
+        [TestCase]
+        public void CheckIfEscrituraValuesAreValid()
+        {
+            ValuesChecker checker = new ValuesChecker();
+            Escritura escritura = new Escritura
+            {
+                NumeroAtencion = 1,
+                CNE = "SampleCNE",
+                Comuna = null,
+                Manzana = "SampleManzana",
+                Predio = "SamplePredio",
+                Fojas = 1,
+                FechaInscripcion = DateTime.Now,
+                NumeroInscripcion = "SampleNumeroInscripcion",
+                Estado = "SampleEstado"
+            };
+
+            bool expectedResult = false;
+            bool actualResult = checker.CheckIfEscrituraValuesAreValid(escritura);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void GetLatestInscriptionNumberOfYear()
+        {
+            ValuesChecker checker = new ValuesChecker();
+            List<Multipropietario> multipropietarios = new List<Multipropietario>
+            {
+                new Multipropietario
+                {
+                    Id = 1,
+                    Comuna = "VITACURA",
+                    Manzana = "1",
+                    Predio = "1",
+                    RunRut = "103394565",
+                    PorcentajeDerecho = 50,
+                    Fojas = 1,
+                    AñoInscripcion = 2020,
+                    NumeroInscripcion = 1,
+                    FechaInscripcion = DateTime.Now,
+                    AñoVigenciaInicial = 2020,
+                    AñoVigenciaFinal = 2022
+                },
+                new Multipropietario
+                {
+                    Id = 2,
+                    Comuna = "LAS CONDES",
+                    Manzana = "2",
+                    Predio = "2",
+                    RunRut = "210339455",
+                    PorcentajeDerecho = 50,
+                    Fojas = 1,
+                    AñoInscripcion = 2020,
+                    NumeroInscripcion = 2,
+                    FechaInscripcion = DateTime.Now,
+                    AñoVigenciaInicial = 2020,
+                    AñoVigenciaFinal = 2022
+                },
+                new Multipropietario
+                {
+                    Id = 3,
+                    Comuna = "PUENTE ALTO",
+                    Manzana = "3",
+                    Predio = "3",
+                    RunRut = "103394557",
+                    PorcentajeDerecho = 50,
+                    Fojas = 1,
+                    AñoInscripcion = 2021,
+                    NumeroInscripcion = 3,
+                    FechaInscripcion = DateTime.Now,
+                    AñoVigenciaInicial = 2021,
+                    AñoVigenciaFinal = 2022
+                }
+            };
+
+            int year = 2020;
+            int expectedInscriptionNumber = 2;
+            int actualInscriptionNumber = checker.GetLatestInscriptionNumberOfYear(multipropietarios, year);
+
+            Assert.AreEqual(expectedInscriptionNumber, actualInscriptionNumber);
         }
     }
 }
