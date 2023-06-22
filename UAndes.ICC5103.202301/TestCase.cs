3d59442ed8f1bfa366582cf36a814d0bd17ef4cb
Ignacio Inostroza <iinostroza1@miuandes.cl>
2023-06-22 17:59:22 -0400

    Test update

--- UAndes.ICC5103.202301/TestCase.cs
+++ UAndes.ICC5103.202301/TestCase.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using UAndes.ICC5103._202301.Models;
 using UAndes.ICC5103._202301.Views;
 
 namespace UAndes.ICC5103._202301
@@ -10,6 +11,34 @@ namespace UAndes.ICC5103._202301
     [TestFixture]
     public class TestCase
     {
+        [TestCase]
+        public void CheckSumAdquirientes()
+        {
+            AdquirienteVerificator verificator = new AdquirienteVerificator();
+            List<AdquirienteClass> adquirientelist = new List<AdquirienteClass>
+            {
+                new AdquirienteClass
+                {
+                    item = 1,
+                    rut = "123456789",
+                    porcentajeDerecho = 70,
+                    porcentajeDerechoNoAcreditado = false
+                },
+                new AdquirienteClass
+                {
+                    item = 2,
+                    rut = "987654321",
+                    porcentajeDerecho = 30,
+                    porcentajeDerechoNoAcreditado = false
+                }
+            };
+
+            decimal expectedSum = 100;
+            decimal actualSum = verificator.SumOfPercentages(adquirientelist);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+
         [TestCase]
         public void ValidPercentage()
         {
@@ -31,11 +60,54 @@ namespace UAndes.ICC5103._202301
                     porcentajeDerechoNoAcreditado = false
                 }
             };
-            bool expectedValidity = true; 
 
+            bool expectedValidity = true; 
             bool actualValidity = checker.CheckIfSumOfPercentagesIsValid(adquirientelist);
 
             Assert.AreEqual(expectedValidity, actualValidity);
+        }
+
+        [TestCase]
+        public void GetLatestMultipropietarioYear_ReturnsLatestYear()
+        {
+            ValuesChecker checker = new ValuesChecker();
+            List<Multipropietario> multipropietarios = new List<Multipropietario>
+            {
+                new Multipropietario
+                {
+                    Id = 1,
+                    Comuna = "VITACURA",
+                    Manzana = "1",
+                    Predio = "1",
+                    RunRut = "123456789",
+                    PorcentajeDerecho = 50,
+                    Fojas = 1,
+                    AñoInscripcion = 2020,
+                    NumeroInscripcion = 1,
+                    FechaInscripcion = DateTime.Now,
+                    AñoVigenciaInicial = 2019,
+                    AñoVigenciaFinal = 2023
+                },
+                new Multipropietario
+                {
+                    Id = 2,
+                    Comuna = "LAS CONDES",
+                    Manzana = "2",
+                    Predio = "2",
+                    RunRut = "987654321",
+                    PorcentajeDerecho = 50,
+                    Fojas = 1,
+                    AñoInscripcion = 2018,
+                    NumeroInscripcion = 1,
+                    FechaInscripcion = DateTime.Now,
+                    AñoVigenciaInicial = 2020,
+                    AñoVigenciaFinal = 2021
+                }
+            };
+            int expectedYear = 2020;
+            int actualYear = checker.GetLatestMultipropietarioYear(multipropietarios);
+
+            Assert.AreEqual(expectedYear, actualYear);
         }
     }
 }
